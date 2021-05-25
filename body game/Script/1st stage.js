77ce084502ce50d8071aa21ef2664707a8e0d317
ahmed-hefny <ahmedhgad@gmail.com>
2021-05-25 14:54:45 +0200

    added games & entertainment files

---
+++ body game/Script/1st stage.js
@@ -0,0 +1,202 @@
+
+var text = document.getElementById("text")
+var description = document.getElementById("description")
+// Hair
+document.getElementById("leg").onclick = function () {
+
+    text.innerHTML = "leg"
+    description.innerHTML = `A leg is a weight-bearing and locomotive anatomical structure, usually having a columnar shape. During locomotion,
+    legs function as "extensible struts". The combination of movements at all joints can be modeled as a single,
+    linear element capable of changing length and rotating about an omnidirectional "hip" joint.
+`
+}
+document.getElementById("foot").onclick = function () {
+
+    text.innerHTML = "Foot"
+    description.innerHTML = `The foot (plural: feet) is an anatomical structure found in many vertebrates. It is the terminal portion of a limb
+    which bears weight and allows locomotion. In many animals with feet, the foot is a separate[clarification needed]
+    organ at the terminal part of the leg made up of one or more segments or bones, generally including claws or nails.
+`
+}
+document.getElementById("body").onclick = function () {
+
+    text.innerHTML = "body"
+    description.innerHTML = `The human body is the structure of a human being. It is composed of many different types of cells that together
+    create tissues and subsequently organ systems. They ensure homeostasis and the viability of the human body.
+    It comprises a head, neck, trunk (which includes the thorax and abdomen), arms and hands, legs and feet.
+    The study of the human body involves anatomy, physiology, histology and embryology. The body varies anatomically in
+    known ways. Physiology focuses on the systems and organs of the human body and their functions. Many systems and
+    mechanisms interact in order to maintain homeostasis, with safe levels of substances such as sugar and oxygen in the
+    blood.`
+}
+document.getElementById("chess").onclick = function () {
+
+    text.innerHTML = "Chest"
+    description.innerHTML = `The thorax or chest is a part of the anatomy of humans, mammals, other tetrapod animals located between the neck and
+    the abdomen. In insects, crustaceans, and the extinct trilobites, the thorax is one of the three main
+    divisions of the creature's body, each of which is in turn composed of multiple segments.
+`
+}
+document.getElementById("ARM").onclick = function () {
+
+    text.innerHTML = "Arm"
+    description.innerHTML = `In human anatomy, the arm is the part of the upper limb between the glenohumeral joint (shoulder joint) and the
+    elbow joint. In common usage, the arm extends through the hand. It can be divided into the upper arm, which extends
+    from the shoulder to the elbow, the forearm which extends from the elbow to the hand, and the hand. Anatomically the
+    shoulder girdle with bones and corresponding muscles is by definition a part of the arm. The Latin term brachium may
+    refer to either the arm as a whole or to the upper arm on its own`
+}
+document.getElementById("hand").onclick = function () {
+
+    text.innerHTML = "Hand"
+    description.innerHTML = `A hand is a prehensile, multi-fingered appendage located at the end of the forearm or forelimb of primates such as
+    humans, chimpanzees, monkeys, and lemurs. A few other vertebrates such as the koala (which has two opposable thumbs
+    on each "hand" and fingerprints extremely similar to human fingerprints) are often described as having "hands"
+    instead of paws on their front limbs. The raccoon is usually described as having "hands" though opposable thumbs are
+    lacking`
+}
+document.getElementById("FINGER").onclick = function () {
+
+    text.innerHTML = "Finger"
+    description.innerHTML = `A finger is a limb of the human body and a type of digit, an organ of manipulation and sensation found in the hands
+    of humans and other primates. Normally humans have five digits, the bones of which are termed phalanges,
+    on each hand, although some people have more or fewer than five due to congenital disorders such as polydactyly or
+    oligodactyly, or accidental or medical amputations. The first digit is the thumb, followed by index finger, middle
+    finger, ring finger, and little finger or pinkie. According to different definitions, the thumb can be called a
+    finger, or not.`
+}
+document.getElementById("stomach").onclick = function () {
+
+    text.innerHTML = "Stomach"
+    description.innerHTML = ` The stomach is a muscular, hollow organ in the gastrointestinal tract of humans and many other animals, including
+    several invertebrates. The stomach has a dilated structure and functions as a vital digestive organ. In the
+    digestive system the stomach is involved in the second phase of digestion, following chewing. It performs a chemical
+    breakdown by means of enzymes and hydrochloric acid.`
+}
+document.getElementById("toe").onclick = function () {
+
+    text.innerHTML = "toe"
+    description.innerHTML = `Toes are the digits (fingers) of the foot of a tetrapod. Animal species such as cats that walk on their toes are
+    described as being digitigrade. Humans, and other animals that walk on the soles of their feet, are described as
+    being plantigrade; unguligrade animals are those that walk on hooves at the tips of their toes.
+`
+}
+document.getElementById("hair2").onclick = function () {
+
+    text.innerHTML = "hair"
+    description.innerHTML = `Hair is a protein filament that grows from follicles found in the dermis. Hair is one of the defining
+    characteristics of mammals. The human body, apart from areas of glabrous skin, is covered in follicles which produce
+    thick terminal and fine vellus hair. Most common interest in hair is focused on hair growth, hair types, and hair
+    care, but hair is also an important biomaterial primarily composed of protein, notably alpha-keratin.
+`
+}
+document.getElementById("hair1").onclick = function () {
+
+    text.innerHTML = "hair"
+    description.innerHTML = `Hair is a protein filament that grows from follicles found in the dermis. Hair is one of the defining
+    characteristics of mammals. The human body, apart from areas of glabrous skin, is covered in follicles which produce
+    thick terminal and fine vellus hair. Most common interest in hair is focused on hair growth, hair types, and hair
+    care, but hair is also an important biomaterial primarily composed of protein, notably alpha-keratin.
+`
+}
+document.getElementById("ear").onclick = function () {
+
+    text.innerHTML = "ear"
+    description.innerHTML = `The ear is the organ of hearing and, in mammals, balance. In mammals, the ear is usually described as having three
+    parts—the outer ear, the middle ear and the inner ear. The outer ear consists of the pinna and the ear canal. Since
+    the outer ear is the only visible portion of the ear in most animals, the word "ear" often refers to the external
+    part alone. The middle ear includes the tympanic cavity and the three ossicles. The inner ear sits in the bony
+    labyrinth, and contains structures which are key to several senses: the semicircular canals, which enable balance
+    and eye tracking when moving; the utricle and saccule, which enable balance when stationary; and the cochlea, which
+    enables hearing. The ears of vertebrates are placed somewhat symmetrically on either side of the head, an
+    arrangement that aids sound localisation.`
+}
+document.getElementById("face").onclick = function () {
+
+    text.innerHTML = "face"
+    description.innerHTML = ` The face is the front of an animal's head that features three of the head's sense organs, the eyes, nose, and mouth,
+    and through which animals express many of their emotions. The face is crucial for human identity, and damage
+    such as scarring or developmental deformities affects the psyche adversely.
+`
+}
+document.getElementById("eye").onclick = function () {
+
+    text.innerHTML = "eye"
+    description.innerHTML = `Eyes are organs of the visual system. They provide animals with vision, the ability to receive and process visual
+    detail, as well as enabling several photo response functions that are independent of vision. Eyes detect light and
+    convert it into electro-chemical impulses in neurons. In higher organisms, the eye is a complex optical system which
+    collects light from the surrounding environment, regulates its intensity through a diaphragm, focuses it through an
+    adjustable assembly of lenses to form an image, converts this image into a set of electrical signals, and transmits
+    these signals to the brain through complex neural pathways that connect the eye via the optic nerve to the visual
+    cortex and other areas of the brain. Eyes with resolving power have come in ten fundamentally different forms, and
+    96% of animal species possess a complex optical system. Image-resolving eyes are present in molluscs, chordates
+    and arthropods`
+}
+document.getElementById("eyebrow").onclick = function () {
+
+    text.innerHTML = "eyebrow"
+    description.innerHTML = `The eyebrow is an area of short hairs above the eye that follows the shape of the lower margin of the brow ridges of
+    some mammals. Their main function is to prevent sweat, water, and other debris from falling down into the eye
+    socket, but they are also important to human communication and facial expression. It is common for people to modify
+    their eyebrows by means of hair removal and makeup.`
+}
+document.getElementById("nose").onclick = function () {
+
+    text.innerHTML = "nose"
+    description.innerHTML = `A nose is a protuberance in vertebrates that houses the nostrils, or nares, which receive and expel air for
+    respiration alongside the mouth. Behind the nose are the olfactory mucosa and the sinuses. Behind the nasal cavity,
+    air next passes through the pharynx, shared with the digestive system, and then into the rest of the respiratory
+    system. In humans, the nose is located centrally on the face and serves as an alternative respiratory passage
+    especially during suckling for infants
+`
+}
+document.getElementById("cheek").onclick = function () {
+
+    text.innerHTML = "Cheek"
+    description.innerHTML = `The cheeks (Latin: buccae) constitute the area of the face below the eyes and between the nose and the left or right
+    ear. "Buccal" means relating to the cheek. In humans, the region is innervated by the buccal nerve. The area between
+    the inside of the cheek and the teeth and gums is called the vestibule or buccal pouch or buccal cavity and forms
+    part of the mouth. In other animals the cheeks may also be referred to as jowls.
+`
+}
+document.getElementById("tounge").onclick = function () {
+
+    text.innerHTML = "Tounge"
+    description.innerHTML = `The tongue is a muscular organ in the mouth of most vertebrates that manipulates food for mastication and is used in
+    the act of swallowing. It has importance in the digestive system and is the primary organ of taste in the gustatory
+    system. The tongue's upper surface (dorsum) is covered by taste buds housed in numerous lingual papillae. It is
+    sensitive and kept moist by saliva and is richly supplied with nerves and blood vessels. The tongue also serves as a
+    natural means of cleaning the teeth. A major function of the tongue is the enabling of speech in humans and
+    vocalization in other animals.`
+}
+document.getElementById("Teeth").onclick = function () {
+    text.innerHTML = "Teeth"
+    description.innerHTML = `A tooth (plural teeth) is a hard, calcified structure found in the jaws (or mouths) of many vertebrates and used to
+    break down food.Some animals, particularly carnivores, also use teeth for hunting or for defensive purposes.The
+    roots of teeth are covered by gums.Teeth are not made of bone, but rather of multiple tissues of varying density
+    and hardness that originate from the embryonic germ layer, the ectoderm.`
+}
+document.getElementById("knee").onclick = function () {
+
+    text.innerHTML = "knee"
+    description.innerHTML = `In humans and other primates, the knee joins the thigh with the leg and consists of two joints: one between the
+        femur and tibia (tibiofemoral joint), and one between the femur and patella (patellofemoral joint). It is the
+        largest joint in the human body. The knee is a modified hinge joint, which permits flexion and extension as well
+        as slight internal and external rotation. The knee is vulnerable to injury and to the development of osteoarthritis.
+    `
+}
+document.getElementById("mouth").onclick = function () {
+
+
+    text.innerHTML = "mouth"
+    description.innerHTML = `In animal anatomy, the mouth, also known as the oral cavity, buccal cavity, or in Latin cavum oris, is the
+        opening through which many animals take in food and issue vocal sounds. It is also the cavity lying at the upper end
+        of the alimentary canal, bounded on the outside by the lips and inside by the pharynx and containing in higher
+        vertebrates the tongue and teeth.This cavity is also known as the buccal cavity, from the Latin bucca
+        ("cheek").`
+}
+
+
+
+
+
---
+++ body game/Script/2nd stage.js
@@ -0,0 +1,180 @@
+
+var text = document.getElementById("text")
+var description1 = document.getElementById("description1")
+var text = document.getElementById("text")
+var description2 = document.getElementById("description2")
+// Hair
+document.getElementById("body").onclick = function () {
+
+    text.innerHTML = "Body"
+    description1.innerHTML = `The human body is the structure of a human being. It is composed of many different types of cells that together
+    create tissues and subsequently organ systems. They ensure homeostasis and the viability of the human body.
+    It comprises a head, neck, trunk (which includes the thorax and abdomen), arms and hands, legs and feet.
+    The study of the human body involves anatomy, physiology, histology and embryology. The body varies anatomically in
+    known ways. Physiology focuses on the systems and organs of the human body and their functions. Many systems and
+    mechanisms interact in order to maintain homeostasis, with safe levels of substances such as sugar and oxygen in the
+    blood.`
+    text2.innerHTML = "Body"
+    description2.innerHTML = `The human body is the structure of a human being. It is composed of many different types of cells that together
+    create tissues and subsequently organ systems. They ensure homeostasis and the viability of the human body.
+    It comprises a head, neck, trunk (which includes the thorax and abdomen), arms and hands, legs and feet.
+    The study of the human body involves anatomy, physiology, histology and embryology. The body varies anatomically in
+    known ways. Physiology focuses on the systems and organs of the human body and their functions. Many systems and
+    mechanisms interact in order to maintain homeostasis, with safe levels of substances such as sugar and oxygen in the
+    blood.`
+}
+document.getElementById("small-intestine").onclick = function () {
+
+    text.innerHTML = "Small-intestine"
+    description1.innerHTML = `  The small intestine or small bowel is an organ in the gastrointestinal tract where most of the end absorption of
+    nutrients and minerals from food takes place. It lies between the stomach and large intestine, and receives bile and
+    pancreatic juice through the pancreatic duct to aid in digestion.The small intestine is about 20 feet (6 meters)
+    long and folds many times to fit in the abdomen. Although it is longer than the large intestine, it is called the
+    small intestine because it is smaller in width.`
+    text2.innerHTML = "Small-intestine"
+    description2.innerHTML = `  The small intestine or small bowel is an organ in the gastrointestinal tract where most of the end absorption of
+    nutrients and minerals from food takes place. It lies between the stomach and large intestine, and receives bile and
+    pancreatic juice through the pancreatic duct to aid in digestion.The small intestine is about 20 feet (6 meters)
+    long and folds many times to fit in the abdomen. Although it is longer than the large intestine, it is called the
+    small intestine because it is smaller in width.`
+}
+document.getElementById("large-intestine").onclick = function () {
+
+    text.innerHTML = "Large-intestine"
+    description1.innerHTML = `The large intestine, also known as the large bowel, is the last part of the gastrointestinal tract and of the
+    digestive system in vertebrates. Water is absorbed here and the remaining waste material is stored as feces before
+    being removed by defecation.`
+    text2.innerHTML = "Large-intestine"
+    description2.innerHTML = `The large intestine, also known as the large bowel, is the last part of the gastrointestinal tract and of the
+    digestive system in vertebrates. Water is absorbed here and the remaining waste material is stored as feces before
+    being removed by defecation.`
+
+}
+document.getElementById("stomach").onclick = function () {
+
+    text.innerHTML = "Stomach"
+    description1.innerHTML = `The stomach is a muscular, hollow organ in the gastrointestinal tract of humans and many other animals, including
+    several invertebrates. The stomach has a dilated structure and functions as a vital digestive organ. In the
+    digestive system the stomach is involved in the second phase of digestion, following chewing. It performs a chemical
+    breakdown by means of enzymes and hydrochloric acid.`
+    text2.innerHTML = "Stomach"
+    description2.innerHTML = `The stomach is a muscular, hollow organ in the gastrointestinal tract of humans and many other animals, including
+    several invertebrates. The stomach has a dilated structure and functions as a vital digestive organ. In the
+    digestive system the stomach is involved in the second phase of digestion, following chewing. It performs a chemical
+    breakdown by means of enzymes and hydrochloric acid.`
+}
+document.getElementById("lung").onclick = function () {
+
+    text.innerHTML = "Lung"
+    description1.innerHTML = `The lungs are the primary organs of the respiratory system in humans and many other animals including a few fish and
+    some snails. In mammals and most other vertebrates, two lungs are located near the backbone on either side of the
+    heart. Their function in the respiratory system is to extract oxygen from the atmosphere and transfer it into the
+    bloodstream, and to release carbon dioxide from the bloodstream into the atmosphere, in a process of gas exchange.
+    Respiration is driven by different muscular systems in different species. Mammals, reptiles and birds use their
+    different muscles to support and foster breathing. In early tetrapods, air was driven into the lungs by the
+    pharyngeal muscles via buccal pumping, a mechanism still seen in amphibians. In humans, the main muscle of
+    respiration that drives breathing is the diaphragm. The lungs also provide airflow that makes vocal sounds including
+    human speech possible.`
+    text2.innerHTML = "Lung"
+    description2.innerHTML = `The lungs are the primary organs of the respiratory system in humans and many other animals including a few fish and
+    some snails. In mammals and most other vertebrates, two lungs are located near the backbone on either side of the
+    heart. Their function in the respiratory system is to extract oxygen from the atmosphere and transfer it into the
+    bloodstream, and to release carbon dioxide from the bloodstream into the atmosphere, in a process of gas exchange.
+    Respiration is driven by different muscular systems in different species. Mammals, reptiles and birds use their
+    different muscles to support and foster breathing. In early tetrapods, air was driven into the lungs by the
+    pharyngeal muscles via buccal pumping, a mechanism still seen in amphibians. In humans, the main muscle of
+    respiration that drives breathing is the diaphragm. The lungs also provide airflow that makes vocal sounds including
+    human speech possible.`
+}
+document.getElementById("liver").onclick = function () {
+
+    text.innerHTML = "Liver"
+    description1.innerHTML = ` The liver is an organ only found in vertebrates which detoxifies various metabolites, synthesizes proteins and
+    produces biochemicals necessary for digestion and growth. In humans, it is located in the right upper
+    quadrant of the abdomen, below the diaphragm. Its other roles in metabolism include the regulation of glycogen
+    storage, decomposition of red blood cells, and the production of hormones.`
+    text2.innerHTML = "Liver"
+    description2.innerHTML = ` The liver is an organ only found in vertebrates which detoxifies various metabolites, synthesizes proteins and
+    produces biochemicals necessary for digestion and growth. In humans, it is located in the right upper
+    quadrant of the abdomen, below the diaphragm. Its other roles in metabolism include the regulation of glycogen
+    storage, decomposition of red blood cells, and the production of hormones.`
+}
+document.getElementById("heart").onclick = function () {
+
+    text.innerHTML = "Heart"
+    description1.innerHTML = `The heart is a muscular organ in most animals, which pumps blood through the blood vessels of the circulatory
+    system.The pumped blood carries oxygen and nutrients to the body, while carrying metabolic waste such as carbon
+    dioxide to the lungs.In humans, the heart is approximately the size of a closed fist and is located between the
+    lungs, in the middle compartment of the chest.`
+    text2.innerHTML = "Heart"
+    description2.innerHTML = `The heart is a muscular organ in most animals, which pumps blood through the blood vessels of the circulatory
+    system.The pumped blood carries oxygen and nutrients to the body, while carrying metabolic waste such as carbon
+    dioxide to the lungs.In humans, the heart is approximately the size of a closed fist and is located between the
+    lungs, in the middle compartment of the chest.`
+}
+document.getElementById("vein").onclick = function () {
+
+    text.innerHTML = "Vein"
+    description1.innerHTML = `Veins are blood vessels that carry blood towards the heart. Most veins carry deoxygenated blood from the tissues
+    back to the heart; exceptions are the pulmonary and umbilical veins, both of which carry oxygenated blood to the
+    heart. In contrast to veins, arteries carry blood away from the heart.
+    Veins are less muscular than arteries and are often closer to the skin. There are valves in most veins to prevent
+    backflow.`
+    text2.innerHTML = "Vein"
+    description2.innerHTML = `Veins are blood vessels that carry blood towards the heart. Most veins carry deoxygenated blood from the tissues
+    back to the heart; exceptions are the pulmonary and umbilical veins, both of which carry oxygenated blood to the
+    heart. In contrast to veins, arteries carry blood away from the heart.
+    Veins are less muscular than arteries and are often closer to the skin. There are valves in most veins to prevent
+    backflow.`
+}
+document.getElementById("kidney").onclick = function () {
+
+    text.innerHTML = "Kidney"
+    description1.innerHTML = `The kidneys are two reddish-brown bean-shaped organs found in vertebrates. They are located on the left and right in
+    the retroperitoneal space, and in adult humans are about 12 centimetres (4 1⁄2 inches) in length. They receive
+    blood from the paired renal arteries; blood exits into the paired renal veins. Each kidney is attached to a ureter,
+    a tube that carries excreted urine to the bladder.`
+    text2.innerHTML = "Kidney"
+    description2.innerHTML = `The kidneys are two reddish-brown bean-shaped organs found in vertebrates. They are located on the left and right in
+    the retroperitoneal space, and in adult humans are about 12 centimetres (4 1⁄2 inches) in length. They receive
+    blood from the paired renal arteries; blood exits into the paired renal veins. Each kidney is attached to a ureter,
+    a tube that carries excreted urine to the bladder.`
+}
+document.getElementById("vessel").onclick = function () {
+
+    text.innerHTML = "Vessel"
+    description1.innerHTML = `The blood vessels are the components of the circulatory system that transport blood throughout the human body.
+    These vessels transport blood cells, nutrients, and oxygen to the tissues of the body. They also take waste and
+    carbon dioxide away from the tissues. Blood vessels are needed to sustain life, because all of the body's tissues
+    rely on their functionality.
+    There are five types of blood vessels: the arteries, which carry the blood away from the heart; the arterioles; the
+    capillaries, where the exchange of water and chemicals between the blood and the tissues occurs; the venules; and
+    the veins, which carry blood from the capillaries back towards the heart.`
+    text2.innerHTML = "Vessel"
+    description2.innerHTML = `The blood vessels are the components of the circulatory system that transport blood throughout the human body.
+    These vessels transport blood cells, nutrients, and oxygen to the tissues of the body. They also take waste and
+    carbon dioxide away from the tissues. Blood vessels are needed to sustain life, because all of the body's tissues
+    rely on their functionality.
+    There are five types of blood vessels: the arteries, which carry the blood away from the heart; the arterioles; the
+    capillaries, where the exchange of water and chemicals between the blood and the tissues occurs; the venules; and
+    the veins, which carry blood from the capillaries back towards the heart.`
+}
+document.getElementById("bladder").onclick = function () {
+
+    text.innerHTML = "Bladder"
+    description1.innerHTML = ` The urinary bladder, or simply bladder, is a hollow muscular organ in humans and other vertebrates that stores urine
+    from the kidneys before disposal by urination. In the human the bladder is a hollow distensible organ that sits on
+    the pelvic floor. Urine enters the bladder via the ureters and exits via the urethra. The typical human bladder will
+    hold between 300 and 500 ml (10.14 and 16.91 fl oz) before the urge to empty occurs, but can hold considerably more
+`
+    text2.innerHTML = "Bladder"
+    description2.innerHTML = ` The urinary bladder, or simply bladder, is a hollow muscular organ in humans and other vertebrates that stores urine
+    from the kidneys before disposal by urination. In the human the bladder is a hollow distensible organ that sits on
+    the pelvic floor. Urine enters the bladder via the ureters and exits via the urethra. The typical human bladder will
+    hold between 300 and 500 ml (10.14 and 16.91 fl oz) before the urge to empty occurs, but can hold considerably more
+`
+}
+
+
+
+
